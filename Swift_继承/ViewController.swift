0cb184f7596aa4d5c1d874998c52ec360163172d
zhaoyangyang <zhaoyangyang@epeizhen.com>
2016-04-07 20:21:13 +0800

    Initial Commit

---
+++ Swift_继承/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift_继承
+//
+//  Created by 赵扬扬 on 16/4/7.
+//  Copyright © 2016年 赵扬扬. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
