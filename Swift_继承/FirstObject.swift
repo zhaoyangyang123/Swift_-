8b888c67fb13cbeaf50dd160b44900dfad4f2514
zhaoyangyang123 <1025221946@qq.com>
2016-12-23 15:12:47 +0800

    创建一个类
    创建一个类

---
+++ Swift_继承/FirstObject.swift
@@ -0,0 +1,13 @@
+//
+//  FirstObject.swift
+//  Swift_继承
+//
+//  Created by 赵扬扬 on 16/12/23.
+//  Copyright © 2016年 赵扬扬. All rights reserved.
+//
+
+import UIKit
+
+class FirstObject: NSObject {
+
+}
